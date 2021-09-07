078294f7e7e397a518872d077085e9f3af689a7d
Mika Patterson <mknpatt21@gmail.com>
2021-09-07 23:25:37 +0000

    Returns unsorted dictionary

--- ratings.py
+++ ratings.py
@@ -7,6 +7,14 @@ def get_ratings(file):
     ratings= {}
     for line in file:
         line = line.rstrip()
-        restaurant, ratings = line.split(':')
-        ratings[restaurant] = ratings
+        # restaurant, ratings = line.split(':')
+        rest_info = line.split(':')
+        restaurant = rest_info[0]
+        rate = rest_info[1]
+        ratings[restaurant] = int(rate)
 
+    sorted_dict = sorted(ratings)
+
+    return sorted_dict
+
+print(get_ratings(the_file))
